c308906a08a127834fd9c23a46d3e0ff3a76514c
Osama Aldalati <osama.aldalati@ivfp.de>
2024-06-22 15:35:04 +0200

    init

---
+++ Main.java
@@ -0,0 +1,142 @@
+import java.util.Random;
+
+public class Main 
+{
+    public static int[][] finalBoard;
+
+    public static int[][] createBoard(int N)
+    {
+        int[][] board = new int[9][9];
+        
+        Random r = new Random();
+
+        for (int i = 0; i < N;)
+        {
+            int num = r.nextInt(9) + 1;
+            int x = r.nextInt(9);
+            int y = r.nextInt(9);
+            
+            if (isSave(board, 9, y, x, num))
+            {
+                board[y][x] = num;
+                i++;
+            }
+        }
+
+        return board;
+
+    }
+
+    public static boolean isSave(int[][] board, int size,  int row, int col, int num)
+    {
+        for (int i = 0; i < size ; i++)
+        {
+            if(board[i][col] == num)
+            {
+                return false;
+            }
+
+            if(board[row][i] == num)
+            {
+                return false;
+            }
+        } 
+
+
+        int boxY = row - row % 3;
+        int boxX = col - col % 3;
+        
+        
+        for (int y = boxY; y < boxY+3; y++)
+        {
+            for (int x = boxX; x < boxX+3; x++)
+            {
+                if(board[y][x] == num)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+
+    public static boolean solve(int[][] board, int y, int x)
+    {
+        if (y >= 9)
+        {
+            return true;
+        }
+
+        if(x >= 9)
+        {
+            return solve(board, y+1, 0);
+        }
+
+        if(board[y][x] != 0)
+        {
+            return solve(board, y, x+1);
+        }
+
+        for (int i = 1; i <= 9; i++)
+        {
+            int[][] copy_board = board.clone();
+            if(isSave(copy_board, 9, y, x, i))
+            {
+                copy_board[y][x] = i;
+                if(solve(copy_board, y, x+1))
+                {
+                    finalBoard = copy_board.clone();
+                    return true;
+                }else
+                {
+                    copy_board[y][x] = 0;
+                };        
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) 
+    {
+
+        var board = createBoard(17);
+
+        for (int i = 0; i < 9 ; i++)
+        {
+           for (int j = 0; j < 9 ; j++)
+           {
+                System.out.print("| "+board[i][j]+" |\t");
+           }
+           System.out.println("\n");
+        }
+
+        System.out.println("\n \n \n \n \n");
+
+        if(solve(board, 0, 0))
+        {
+            for (int i = 0; i < 9 ; i++)
+            {
+               for (int j = 0; j < 9 ; j++)
+               {
+                    System.out.print("| "+finalBoard[i][j]+" |\t");
+               }
+               System.out.println("\n");
+            }   
+        }
+        else
+        {
+            System.out.println("NO NO NO NO NO :(");
+        }
+
+    }
+    
+}
+
+
+
+
+
+
